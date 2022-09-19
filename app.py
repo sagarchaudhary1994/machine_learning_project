c1bd387e80c6c77e15d10204c9f11584d207276f
Sagar Chaudhary <sagarchaudhary1994@gmail.com>
2022-09-19 22:20:26 +0530

    Added Logger and Exception packages

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask
-from 
-
+from housing.logger import logging
+from housing.exception import Housing_Exception
+import sys
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return "<p>CICD is done for this project</p>"
+    try:
+        raise Exception("We are testing exception module")
+    except Exception as e:
+        housing_obj = Housing_Exception(e, sys)
+        logging.info(housing_obj.errr_message)
+        logging.info("Testing logging module")
+        return "<p>CICD is done for this project</p>"
 
 
 if __name__ == "__main__":
---
+++ housing/exception/__init__.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+
+class Housing_Exception(Exception):
+
+    def __init__(self, error_message: Exception, error_detail: sys):
+        super().__init__(error_message)
+        self.errr_message = Housing_Exception.get_detailed_error_message(
+            error_message, error_detail)
+
+    @staticmethod
+    def get_detailed_error_message(error_message: Exception, error_detail: sys) -> str:
+        """
+        error_message: Exception object
+        error_detail: object of sys module
+        """
+        _, _, traceback = error_detail.exc_info()
+        file_name = traceback.tb_frame.f_code.co_filename
+        line_num = traceback.tb_frame.f_lineno
+        error_message = f"error occured in script [{file_name}] at line number [{line_num}] error message [{error_message}]"
+
+        return error_message
+
+    def __str__(self):
+        return self.errr_message
+
+    def __repr__(self):
+        return Housing_Exception.__name__.str()
