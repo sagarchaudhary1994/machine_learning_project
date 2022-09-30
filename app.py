85524ce2965e8781541808b5cc5188e3d271c3f6
Sagar Chaudhary <sagarchaudhary1994@gmail.com>
2022-09-30 22:22:34 +0530

    Template changes

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from genericpath import exists, isfile
 from flask import Flask, request, abort, send_file, render_template
 from housing.logger import logging
 from housing.exception import Housing_Exception
@@ -68,6 +69,111 @@ def predict():
             }
             return render_template('predict.html', context=context)
         return render_template('predict.html', context=context)
+    except Exception as e:
+        raise Housing_Exception(e, sys) from e
+
+
+@app.route('/saved_models', defaults={'req_path': 'housing/artifact/saved_models'})
+@app.route('/saved_models/<path:req_path>')
+def saved_models(req_path):
+    try:
+        os.makedirs(os.path.join("housing", "artifact",
+                    "saved_models"), exist_ok=True)
+        # Joining the base and req_path
+        print(f"req_path: {req_path}")
+        abs_path = os.path.join(req_path)
+        print(abs_path)
+        # Return 404 if path does not exist
+        if not os.path.exists(abs_path):
+            return abort(404)
+
+        # Check is path is file and serve it
+
+        if os.path.isfile(abs_path):
+            return send_file(abs_path)
+
+        # show directory content
+        files = {os.path.join(abs_path, file)                 : file for file in os.listdir(abs_path)}
+
+        result = {
+            "files": files,
+            "parent_folder": os.path.dirname(abs_path),
+            "parent_label": abs_path
+        }
+
+        return render_template("saved_models_files.html", result=result)
+    except Exception as e:
+        raise Housing_Exception(e, sys) from e
+
+
+@app.route('/housing_logs', defaults={'req_path': 'housing_logs'})
+@app.route('/housing_logs/<path:req_path>')
+def logs(req_path):
+    try:
+        os.makedirs("housing_logs", exist_ok=True)
+        # Joining the base and req_path
+        print(f"req_path: {req_path}")
+        abs_path = os.path.join(req_path)
+        print(abs_path)
+
+        # Return 404 if path does not exist
+        if not os.path.exists(abs_path):
+            return abort(404)
+
+        # Check is path is file and serve it
+
+        if os.path.isfile(abs_path):
+            return send_file(abs_path)
+
+        # show directory content
+        files = {os.path.join(abs_path, file): file for file in os.listdir(abs_path)}
+
+        result = {
+            "files": files,
+            "parent_folder": os.path.dirname(abs_path),
+            "parent_label": abs_path
+        }
+
+        return render_template("log_files.html", result=result)
+    except Exception as e:
+        raise Housing_Exception(e, sys) from e
+
+
+@app.route('/artifact', defaults={'req_path': 'housing/artifact'})
+@app.route('/artifact/<path:req_path>')
+def artifact(req_path):
+    try:
+        os.makedirs("housing", exist_ok=True)
+        # Joining the base and req_path
+        print(f"req_path: {req_path}")
+        abs_path = os.path.join(req_path)
+        print(abs_path)
+
+        # Return 404 if path does not exist
+        if not os.path.exists(abs_path):
+            return abort(404)
+
+        # Check is path is file and serve it
+
+        if os.path.isfile(abs_path):
+            if ".html" in abs_path:
+                with open(abs_path, "r", encoding="utf-8") as file:
+                    content = ""
+                    for line in file.readlines():
+                        content = f"{content}{line}"
+                    return content
+
+        # show directory content
+        files = {os.path.join(abs_path, file)
+                              : file for file in os.listdir(abs_path)}
+
+        result = {
+            "files": files,
+            "parent_folder": os.path.dirname(abs_path),
+            "parent_label": abs_path
+        }
+
+        return render_template("files.html", result=result)
     except Exception as e:
         raise Housing_Exception(e, sys) from e
 
